cffeef98460255cf2f43edcfceb85a4bf2180232
Ben Simondet <simon998@morris.umn.edu>
2015-01-27 13:52:17 -0600

    Added more properties to the gpa controller. Need to display list and add gpa functions.

--- public/javascript/main.controller.js
+++ public/javascript/main.controller.js
@@ -15,7 +15,33 @@ var mainApp = angular.module("mainApp", []);
 
 
 //==================== GPA CONTROLLER ====================================
+    mainApp.controller('gpaCtrl', function($scope){
+        $scope.classField = "";
 
+        $scope.gradeField = "";
+
+        $scope.creditField = "";
+
+        $scope.data = [
+            {class: Example, grade: "A", credits: "0"}
+        ];
+
+        $scope.addClass = function(){
+            if($scope.classField.length >= 1 && $scope.gradeField == 1) {
+                $scope.data.push({class: $scope.classField, grade: $scope.gradeField, credits: $scope.creditField});
+                $scope.classField = "";
+                $scope.gradeField = "";
+                $scope.creditField = "";
+            }
+        };
+        $scope.removeData = function(index){
+            $scope.data.splice(index, 1);
+        };
+
+        $scope.classesInList = function(){
+            return $scope.data.length;
+        };
+    });
 
 
 
